02e627ad08735eb85cfe83af32612516d23b0d9a
jleluyer <jeremy.le.luyer@gmail.com>
2017-05-09 22:45:58 -0400

    first commit

---
+++ 00_scripts/plot_circos.R
@@ -0,0 +1,44 @@
+################OmicCircos test ############
+####################################################
+ls()
+rm(list=ls())
+ls()
+
+
+#source("https://bioconductor.org/biocLite.R")
+#biocLite("OmicCircos")
+library(OmicCircos)
+
+
+options(stringsAsFactors = FALSE);
+
+#### Test real data
+temp.seg.f<-read.table("02_data/example_init.seg.txt",header=T)
+data_label_methyl<-read.table("02_data/example_gene.label.txt",header=T)
+data_value_methyl<-read.table("02_data/example_data.methyl.txt",header=T)
+
+#Initialize value
+seg.name <- temp.seg.f[,1]
+seg.num   <- length(unique(temp.seg.f[,1]));
+head(temp.seg.f)
+
+
+# Create df
+db <- segAnglePo(temp.seg.f, seg=seg.name);
+
+
+#Define colors
+colors   <- rainbow(seg.num, alpha=0.5);
+
+
+#plot
+jpeg(filename = "03_results/circos_plot.jpeg",
+     width = 11, height = 11, units = 'in', res = 400)
+yc=475
+xc=475
+par(mar=c(1,1, 1, 1));
+plot(c(1,950), c(1,950), type="n", axes=FALSE, xlab="", ylab="", main="");
+circos(R=420, type="chr", cir=db, col=colors, print.chr.lab=TRUE,xc=xc,yc=yc,W=4,scale=TRUE);
+circos(R=360, cir=db , W=50, mapping=data_value_methyl , col.v=3,xc=xc,yc=yc, type="b2", B=TRUE, cutoff=0,col=c("red","dodgerblue4"),lwd=3);
+circos(R=360, cir=db, W=20, mapping=data_label_methyl, type="label",xc=xc,yc=yc, side="in", col=c("black"), cex=0.8);
+dev.off()
